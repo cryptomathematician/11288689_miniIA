b7e940eb650938e01c5040d292971ac7d6b518e2
cryptomathematician <bayatosman827@gmail.com>
2023-12-18 09:20:28 +0000

    created my information model

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express= require('express')
 const mongoose = require('mongoose')
-const Visitaion = require('./models/visitationModel')
+const Visitation = require('./models/visitationModel')
 const Register = require('./models/registerModel')
+const Information = require('./models/registerModel')
 const app = express()
 
 
@@ -23,6 +24,15 @@ app.post ('/register',async(req, res) =>{
     try{
        const register = await Register.create(req.body)
        res.status(200).json(register);
+    } catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+app.post ('/information',async(req, res) =>{
+    try{
+       const information = await Information.create(req.body)
+       res.status(200).json(information);
     } catch(error){
         console.log(error.message);
         res.status(500).json({message: error.message})
